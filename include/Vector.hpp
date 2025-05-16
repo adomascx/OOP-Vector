e44a4c82a90b1a3fbe53d9b9a9ad4b63b9207de1
adomascx <adlukosev2@gmail.com>
2025-05-16 09:59:04 +0300

    Prideti esminius kintamuosius, default konstruktoriu ir destruktoriu

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "export.h"
 #include <cstddef>
 #include <stdexcept>
 #include <utility>
@@ -9,11 +10,24 @@ template <typename T>
 class VECTOR_API Vector
 {
 public:
-    
+    using size_type = std::size_t;
+
+    // default konstruktorius
+    Vector() noexcept
+        : data_(nullptr), size_(0), capacity_(0) {}
+
+    // destruktorius
+    ~Vector()
+    {
+        delete[] data_;
+    }
+
 private:
-    
+    T *data_;
+    size_type size_;
+    size_type capacity_;
 };
 
 #ifdef BUILDING_VECTOR
-# include "Vector-inl.hpp"
+#include "Vector-inl.hpp"
 #endif
